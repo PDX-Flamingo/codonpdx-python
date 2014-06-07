031287e5b890cb4c1167121ae5aea730d7375c30
William Van Hevelingen <blkperl@cat.pdx.edu>
2014-06-07 16:03:57 -0700

    rename acid to codon

--- count.py
+++ count.py
@@ -1,14 +1,13 @@
 from collections import Counter
 from Bio import SeqIO
 from Bio.SeqUtils import GC
-#for seq_record in SeqIO.parse("/home/blkperl/Downloads/cow.rna.fna", "fasta"):
 for seq_record in SeqIO.parse("samples/test.fasta", "fasta"):
     print seq_record.id
     cnt = Counter()
     line = seq_record.seq
     n = 3
-    for acid in [line[i:i+n] for i in range(0, len(line), n)]:
-      cnt[str(acid)] += 1
+    for codon in [line[i:i+n] for i in range(0, len(line), n)]:
+      cnt[str(codon)] += 1
     print cnt
 
 
