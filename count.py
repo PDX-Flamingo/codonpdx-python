22b6225d5706bf579f028eb269bc7cbbb95f8185
William Van Hevelingen <blkperl@cat.pdx.edu>
2014-06-07 17:22:33 -0700

    Add json output

--- count.py
+++ count.py
@@ -1,13 +1,18 @@
+import json
+
 from collections import Counter
 from Bio import SeqIO
 from Bio.SeqUtils import GC
 for seq_record in SeqIO.parse("samples/test.fasta", "fasta"):
-    print seq_record.id
     cnt = Counter()
     line = str(seq_record.seq)
     i = 0
     for j in range(((len(line))/ 3)):
       cnt[line[i:i+3]] += 1
       i += 3
-    print cnt
+    print json.dumps(
+      [{
+      "id"          : seq_record.id,
+      "ratios"      : cnt
+      }], sort_keys=True, indent=4, separators=(',', ': '))
 
