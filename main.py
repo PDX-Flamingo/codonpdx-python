c7888127f97f9deffb559636d71b9e20d2dc82d4
William Van Hevelingen <blkperl@cat.pdx.edu>
2014-06-09 19:26:33 -0700

    Default to stdin for input files

--- main.py
+++ main.py
@@ -5,22 +5,22 @@ import sys
 import count
 
 def main(argv):
-   inputfile = 'samples/test.fasta'
+   inputfile = sys.stdin
    format = 'fasta'
    try:
       opts, args = getopt.getopt(argv,"hi:f:",["ifile=","format="])
    except getopt.GetoptError:
-      print 'count.py -i <inputfile> -f <format>'
+      print ('count.py -i <inputfile> -f <format>')
       sys.exit(2)
    for opt, arg in opts:
       if opt == '-h':
-         print 'count.py -i <inputfile> -f <format>'
+         print ('count.py -i <inputfile> -f <format>')
          sys.exit()
       elif opt in ("-i", "--ifile"):
          inputfile = arg
       elif opt in ("-f", "--format"):
          format = arg
-   print 'Input file is', inputfile
+   print ('Input file is', inputfile)
 
    count.compute(inputfile,format)
 
