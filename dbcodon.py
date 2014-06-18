52518f6efff2b97d0762e51b4cdc1caa10e43ecf
William Van Hevelingen <blkperl@cat.pdx.edu>
2014-06-17 22:51:23 -0700

    Add config file for db configuration and fix insert bug

--- dbcodon.py
+++ dbcodon.py
@@ -1,20 +1,28 @@
 #!/usr/bin/env python
 
 
+import ConfigParser
 import sys, json
 import psycopg2
 
+config = ConfigParser.RawConfigParser()
+config.read('db.cfg')
+
+host = config.get('database', 'host')
+dbname = config.get('database', 'dbname')
+user = config.get('database', 'user')
+password = config.get('database', 'password')
+
 data = json.load(sys.stdin)
-conn = psycopg2.connect("host=localhost dbname=pdxcodon user=pdxcodon password=secret")
+connection_string = 'host={host} dbname={dbname} user={user} password={password}'.format(**locals())
+conn = psycopg2.connect(connection_string)
 cur = conn.cursor()
 REFSEQ_SQL = "INSERT INTO refseq (pid, id, name, description) VALUES (%s, %s, %s, %s);"
 REFSEQ_COUNT_SQL = "INSERT INTO refseq_counts (refseq_id, codon, count) VALUES (%s, %s, %s);"
-cur.execute("SELECT nextval('refseq_id');")
-refseq_id = int(cur.fetchone()[0])
-
-print "refseq id is", refseq_id
 
 for org in data:
+    cur.execute("SELECT nextval('refseq_id');")
+    refseq_id = int(cur.fetchone()[0])
     name = str(org['name'])
     id = str(org['id'])
     desc = str(org['description'])
