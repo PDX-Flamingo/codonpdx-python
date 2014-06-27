7ada3e3670a04e1092254c81e967946e8c0f1138
William Van Hevelingen <blkperl@cat.pdx.edu>
2014-06-27 16:05:59 -0700

    Update dbcodon for new schema

--- dbcodon.py
+++ dbcodon.py
@@ -17,18 +17,14 @@ data = json.load(sys.stdin)
 connection_string = 'host={host} dbname={dbname} user={user} password={password}'.format(**locals())
 conn = psycopg2.connect(connection_string)
 cur = conn.cursor()
-REFSEQ_SQL = "INSERT INTO refseq (pid, id, name, description) VALUES (%s, %s, %s, %s);"
-REFSEQ_COUNT_SQL = "INSERT INTO refseq_counts (refseq_id, codon, count) VALUES (%s, %s, %s);"
+SQL = "INSERT INTO refseq2 (id, name, description, ACT, ACG, ACA, ACC, ATC, ATG, ATA, ATT, AGC, AGT, AGA, AGG, AAC, AAT, AAG, AAA, CAT, CAG, CAA, CAC, CTA, CTG, CTC, CTT, CGA, CGT, CGC, CGG, CCA, CCT, CCG, CCC, TAC, TAG, TAA, TAT, TCA, TCG, TCC, TCT, TGA, TGC, TGT, TGG, TTA, TTC, TTG, TTT, GAC, GAT, GAA, GAG, GCA, GCT, GCC, GCG, GTA, GTC, GTT, GTG, GGA, GGC, GGT, GGG) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s);"
 
 for org in data:
-    cur.execute("SELECT nextval('refseq_id');")
-    refseq_id = int(cur.fetchone()[0])
-    name = str(org['name'])
     id = str(org['id'])
+    name = str(org['name'])
     desc = str(org['description'])
-    cur.execute(REFSEQ_SQL, (refseq_id,id,name,desc))
-    for k,v in org['codoncount'].items():
-        cur.execute(REFSEQ_COUNT_SQL, (refseq_id,k,int(v)))
+    cc = org['codoncount']
+    cur.execute(SQL, (id,name,desc, int(cc['AAA']),int(cc['AAC']),int(cc['AAG']),int(cc['AAT']),int(cc['ACA']),int(cc['ACC']),int(cc['ACG']),int(cc['ACT']),int(cc['AGA']),int(cc['AGC']),int(cc['AGG']),int(cc['AGT']),int(cc['ATA']),int(cc['ATC']),int(cc['ATG']),int(cc['ATT']),int(cc['CAA']),int(cc['CAC']),int(cc['CAG']),int(cc['CAT']),int(cc['CCA']),int(cc['CCC']),int(cc['CCG']),int(cc['CCT']),int(cc['CGA']),int(cc['CGC']),int(cc['CGG']),int(cc['CGT']),int(cc['CTA']),int(cc['CTC']),int(cc['CTG']),int(cc['CTT']),int(cc['GAA']),int(cc['GAC']),int(cc['GAG']),int(cc['GAT']),int(cc['GCA']),int(cc['GCC']),int(cc['GCG']),int(cc['GCT']),int(cc['GGA']),int(cc['GGC']),int(cc['GGG']),int(cc['GGT']),int(cc['GTA']),int(cc['GTC']),int(cc['GTG']),int(cc['GTT']),int(cc['TAA']),int(cc['TAC']),int(cc['TAG']),int(cc['TAT']),int(cc['TCA']),int(cc['TCC']),int(cc['TCG']),int(cc['TCT']),int(cc['TGA']),int(cc['TGC']),int(cc['TGG']),int(cc['TGT']),int(cc['TTA']),int(cc['TTC']),int(cc['TTG']),int(cc['TTT'])))
 
 
 conn.commit()
