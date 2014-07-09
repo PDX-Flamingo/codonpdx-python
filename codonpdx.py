8ffae4caf3affb79a94bf396979c7bb8eb6c4254
Cameron Hobbs <masamune-7@hotmail.com>
2014-07-09 15:45:31 -0700

    Add some detail to the help messages given by the program.

--- codonpdx.py
+++ codonpdx.py
@@ -8,41 +8,41 @@ from codonpdx.insert import loadDB
 
 # create the top-level parser
 parser = argparse.ArgumentParser(prog='codonpdx',
-                                 description='codonpdx command line util')
+                                 description='Codonpdx command line utility.')
 parser.add_argument('--version', action='version', version='%(prog)s 1.0')
-subparsers = parser.add_subparsers(help='sub-command help')
+subparsers = parser.add_subparsers(help='Sub-command descriptions:')
 
 # create the parser for the "count" command
-parserCount = subparsers.add_parser('count', help='codon counter')
+parserCount = subparsers.add_parser('count', help='Count the codons of a file and produce JSON output containing the results.')
 parserCount.add_argument('-i', '--infile', nargs='?',
                          type=argparse.FileType('r'), default=sys.stdin,
-                         help='a file path')
+                         help='A file containing sequence data.')
 parserCount.add_argument('-f', '--format', choices=['fasta', 'genbank'],
-                         help='the file format')
+                         help='The file format.')
 parserCount.add_argument('-p', '--pretty', action='store_true',
-                         help='enable json pretty print')
+                         help='Print the JSON in a pretty, more human-readable way.')
 parserCount.set_defaults(func=codonCount)
 
 # create the parser for the "loadDB" command
-parserLoadDB = subparsers.add_parser('loadDB',
-                                     help='stores codon count metadata')
+parserLoadDB = subparsers.add_parser('insert',
+                                     help='Insert organism codon count JSON information into the database.')
 parserLoadDB.add_argument('-d', '--dbname', choices=['refseq', 'genbank'],
-                          help='the dbname to store the count metadata')
+                          help='The database table to store the count information in.')
 parserLoadDB.add_argument('-i', '--infile', nargs='?',
                           type=argparse.FileType('r'), default=sys.stdin,
-                          help='a file path')
+                          help='The file to to read the JSON data from. Defaults to standard input.')
 parserLoadDB.set_defaults(func=loadDB)
 
 
 # create the parser for the "calcScore" command
 parserCalcScore = subparsers.add_parser(
-    'calcScore',
-    help='calculate the codon count scores'
+    'calc',
+    help='Compare an organism to all other organisms in a given sequence database.'
 )
 parserCalcScore.add_argument('-d', '--dbname', choices=['refseq', 'genbank'],
-                             help='the dbname to fetch the count metadata')
+                             help='The sequence database to compare the organism to.')
 parserCalcScore.add_argument('-v', '--virus', required=True,
-                             help='the virus id to compare against')
+                             help='The accession.version number of the organism to compare. (Currently must be located in the sequence database to compare against.)')
 parserCalcScore.set_defaults(func=calcScore)
 
 args = parser.parse_args()
